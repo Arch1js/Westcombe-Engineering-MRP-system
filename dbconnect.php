77a57adef9f2fab992a91a25083b5780fc039b28
Arch1js <a.dobrajs@gmail.com>
2016-05-18 12:50:22 +0100

    Added search, add, edit and sort functions to orders table

--- dbconnect.php
+++ dbconnect.php
@@ -2,7 +2,7 @@
 
 $host = "wedatabase.cr3lqvgcf76h.us-west-2.rds.amazonaws.com";
 $user = "archijs";
-$password = "Artu3005";
+$password = "y9wWVqSue9wX9JpM";
 $db = "WE_database";
 
 $mysqli=mysqli_connect($host,$user,$password,$db);
--- js/directives.js
+++ js/directives.js
@@ -1,6 +1,12 @@
 app.directive("navbar", function () {
 	return {
 		restrict: "A",
-		templateUrl: "/views/nav-bar.php"
+		templateUrl: "../views/nav-bar.php"
+	}
+});
+app.directive("loading", function () {
+	return {
+		restrict: "A",
+		templateUrl: "../views/loading_spinner.html"
 	}
 });
